779afe225eecb67226c67ecc4e9cad887cb8985a
Seth Nadu <seth.nadu@gmail.com>
2020-01-15 14:26:41 -0500

    miner working, infinite loop problem

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -46,7 +46,7 @@ class Blockchain(object):
         # Return the new block
         return block
 
-    def hash(block):
+    def hash(self, block):
         """
         Creates a SHA-256 hash of a Block
 
@@ -91,8 +91,11 @@ class Blockchain(object):
         :return: A valid proof for the provided block
         """
         # TODO
-        pass
-        # return proof
+        block_string = json.dumps(block, sort_keys=True)
+        proof = 0
+        while self.valid_proof(block_string, proof) is False:
+            proof += 1
+        return proof
 
     @staticmethod
     def valid_proof(block_string, proof):
@@ -107,7 +110,10 @@ class Blockchain(object):
         :return: True if the resulting hash is a valid proof, False otherwise
         """
         # TODO
-        pass
+        guess = f"{block_string}{proof}".encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:3] == '000'
         # return True or False
 
 
@@ -124,11 +130,12 @@ blockchain = Blockchain()
 @app.route('/mine', methods=['GET'])
 def mine():
     # Run the proof of work algorithm to get the next proof
-
+    proof = blockchain.proof_of_work(blockchain.last_block)
     # Forge the new Block by adding it to the chain with the proof
-
+    previous_hash = blockchain.hash(blockchain.last_block)
+    block = blockchain.new_block(proof, previous_hash)
     response = {
-        # TODO: Send a JSON response with the new block
+        "new_block": block
     }
 
     return jsonify(response), 200
--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -13,7 +13,12 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(new_proof, sort_keys=True)
+    proof = 0
+    while self.valid_proof(block_string, proof) is False:
+        proof += 1
+    return proof
+    
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +32,10 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f"{block_string}{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    return guess_hash[:6] == '000000'
 
 
 if __name__ == '__main__':
@@ -45,6 +53,7 @@ if __name__ == '__main__':
 
     # Run forever until interrupted
     while True:
+        coins = 0
         r = requests.get(url=node + "/last_block")
         # Handle non-json response
         try:
@@ -56,15 +65,20 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        new_proof = data["last_block"]
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
 
-        r = requests.post(url=node + "/mine", json=post_data)
-        data = r.json()
+        re = requests.post(url=node + "/mine", json=post_data)
+        new_data = re.json()
 
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        print("MESSAGE", new_data[0]["message"])
+        if new_data[0]["message"] == "Success":
+            coins += 1
+            print(f'Coins mined: {coins}')
+        else:
+            print(new_data[0]["message"])
